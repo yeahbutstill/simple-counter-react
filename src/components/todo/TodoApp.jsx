c4321aa62bf94e44f096ca88329155d384008fce
Dani Setiawan <mayadani28@gmail.com>
2023-01-29 00:28:13 +0700

    implement todo component

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -5,12 +5,12 @@ import {useState} from "react";
 export default function TodoApp() {
     return (
         <div className="TodoApp">
-            Todo Management Application
             <BrowserRouter>
                 <Routes>
                     <Route path='/' element={<LoginComponent/>}></Route>
                     <Route path='/login' element={<LoginComponent/>}></Route>
                     <Route path='/welcome/:username' element={<WelcomeComponent/>}></Route>
+                    <Route path='/todos' element={<ListTodosComponent/>}></Route>
                     <Route path='*' element={<ErrorComponent/>}></Route>
                 </Routes>
             </BrowserRouter>
@@ -87,6 +87,46 @@ function ErrorComponent() {
             <h1>We are working reall hard!</h1>
             <div>
                 Apologies for the 404. Reach out to our team at ABC-DEF-GHIJ.
+            </div>
+        </div>
+    )
+}
+
+function ListTodosComponent() {
+
+    const todos = [
+        {id: 1, description: 'Learn AWS', username: 'yeahbutstill'},
+        {id: 2, description: 'Learn Vault', username: 'yeahbutstill'},
+        {id: 3, description: 'Learn Keycloak', username: 'yeahbutstill'},
+        {id: 4, description: 'Learn Docker', username: 'yeahbutstill'}
+    ]
+
+    return (
+        <div className="ListTodosComponent">
+            <h1>Things you want to do!</h1>
+            <div>
+                <table>
+                    <thead>
+                    <tr>
+                        <td>id</td>
+                        <td>description</td>
+                        <td>username</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        todos.map(
+                            todo => (
+                                <tr key={todo.id}>
+                                    <td>{todo.id}</td>
+                                    <td>{todo.description}</td>
+                                    <td>{todo.username}</td>
+                                </tr>
+                            )
+                        )
+                    }
+                    </tbody>
+                </table>
             </div>
         </div>
     )
