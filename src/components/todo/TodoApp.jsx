8c546fe04516b2d1af19f40b4c0aa52d98497d2b
Dani Setiawan <mayadani28@gmail.com>
2023-01-28 23:46:08 +0700

    implement error page

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -11,6 +11,7 @@ export default function TodoApp() {
                     <Route path='/' element={<LoginComponent/>}></Route>
                     <Route path='/login' element={<LoginComponent/>}></Route>
                     <Route path='/welcome' element={<WelcomeComponent/>}></Route>
+                    <Route path='*' element={<ErrorComponent/>}></Route>
                 </Routes>
             </BrowserRouter>
         </div>
@@ -46,6 +47,7 @@ function LoginComponent() {
 
     return (
         <div className="Login">
+            <h1>Time to Login!</h1>
             {showSuccessMessage && <div className="successMessage">Authenticated Successfully</div>}
             {showErrorMessage && <div className="errorMessage">Authenticated Failed, please check your credentials.</div>}
             <div className="LoginForm">
@@ -67,8 +69,22 @@ function LoginComponent() {
 
 function WelcomeComponent() {
     return (
-        <div className="Welcome">
-            Welcome Component
+        <div className="WelcomeComponent">
+            <h1>Welcome yeahbutstill</h1>
+            <div>
+                Welcome Component
+            </div>
+        </div>
+    )
+}
+
+function ErrorComponent() {
+    return (
+        <div className="ErrorComponent">
+            <h1>We are working reall hard!</h1>
+            <div>
+                Apologies for the 404. Reach out to our team at ABC-DEF-GHIJ.
+            </div>
         </div>
     )
 }
